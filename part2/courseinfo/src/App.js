843e02a666183719624cbc113813477034b07293
Jan Haubrock <jan.haubrock@live.com>
2022-03-24 21:21:09 +0100

    courseinfo part2 initial

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,68 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part) => (
+        <Part name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach((e) => (total = total + e.exercises));
+
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application developtment",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
