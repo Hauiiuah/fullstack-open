430a76290c6eeb7610271fb24ff93bd33589b629
Jan Haubrock <jan.haubrock@live.com>
2022-03-24 21:33:25 +0100

    2.1 Courseinfo finished

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,34 @@
-const Header = (props) => {
+const Header = ({course}) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </>
   );
 };
 
-const Part = (props) => {
+const Part = ({name,exercises}) => {
   return (
     <>
       <p>
-        {props.name} {props.exercises}
+        {name} {exercises}
       </p>
     </>
   );
 };
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <>
-      {props.parts.map((part) => (
+      {parts.map((part) => (
         <Part name={part.name} exercises={part.exercises} />
       ))}
     </>
   );
 };
 
-const Total = (props) => {
+const Total = ({parts}) => {
   let total = 0;
-  props.parts.forEach((e) => (total = total + e.exercises));
+  parts.forEach((e) => (total = total + e.exercises));
 
   return (
     <>
@@ -37,25 +37,8 @@ const Total = (props) => {
   );
 };
 
-const App = () => {
-  const course = {
-    name: "Half Stack application developtment",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
 
+const Course = ({course}) => {
   return (
     <div>
       <Header course={course.name} />
@@ -63,6 +46,37 @@ const App = () => {
       <Total parts={course.parts} />
     </div>
   );
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application developtment",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id:1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id:2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id:3
+      },
+      {
+        name: "Eventlistener",
+        exercises: 3,
+        id: 4
+      }
+    ],
+  };
+
+  return <Course course={course} />
 };
 
 export default App;
