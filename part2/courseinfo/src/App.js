a1b2e41dcdc41f3efebb6cacf65a3b49c7687622
Jan Haubrock <jan.haubrock@live.com>
2022-03-24 22:11:06 +0100

    2.5. create separate course module, fix typo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,59 +1,11 @@
-const Header = ({course}) => {
-  return (
-    <>
-      <h1>{course}</h1>
-    </>
-  );
-};
-
-const Part = ({name,exercises}) => {
-  return (
-    <>
-      <p>
-        {name} {exercises}
-      </p>
-    </>
-  );
-};
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part name={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({parts}) => {
-  
-  let total=parts.reduce((prev,cur)=> prev + cur.exercises ,0)
-
-  return (
-    <>
-      <p><b>Number of exercises {total}</b></p>
-    </>
-  );
-};
-
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
+import Course from "./components/Course";
 
 const App = () => {
   const courses = 
   [
     {
     id: 1,
-    name: "Half Stack application developtment",
+    name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
