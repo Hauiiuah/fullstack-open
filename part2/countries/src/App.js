8f7e4e3652a4f7b0559e375af77afa7ff5231b5c
Jan Haubrock <jan.haubrock@live.com>
2022-03-25 20:55:58 +0100

    2.12 countries basic functionality

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,8 +1,65 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const App = () => {
+
+
+const Display = ({country}) => {
+
+  console.log(country)
+
   return (
     <>
-      Hello world
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area: {country.area}</p>
+      <h2>languages</h2>
+      <ul>
+        {Object.values(country.languages).map( lang => <li key={lang}>{lang}</li>)}
+      </ul>
+      <img src={country.flags.png} />
+    </>
+  )
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [countrieFilter, setCountrieFilter]=useState('')
+
+
+  const filtered = countrieFilter ? countries.filter(country => country.name.common.toLowerCase().includes(countrieFilter.toLowerCase())) : countries
+ 
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(result => setCountries(result.data))
+  },[])
+
+
+
+  let display
+  if(filtered.length > 10){
+    display=<p>Too many matches, specify another filter</p>
+  }else if(filtered.length ==1 )
+  {
+    display=<Display country={filtered[0]} />
+  }else
+  {
+    display=filtered.map( country => <p key={country.name.common}>{country.name.common}</p>)
+  }
+
+
+  
+
+  return (
+    <>
+      find countries <input value={countrieFilter} onChange={(event) => setCountrieFilter(event.target.value)} />
+
+      <hr />
+      {display}
+
+      
+
     </>
   )
 }
